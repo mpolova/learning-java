a2644a9853cc7705e1585a1d70c05aa3ec21efb0
Mariia Polova <mariia.poliova@gmail.com>
2026-07-13 19:14:13 +0200

    Add string concatenation exercise

---
+++ Introduction to Java/StringConcatenation.java
@@ -0,0 +1,10 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String itemName = "VR Headset";
+        int itemPrice = 599;
+
+        System.out.println("Item is " + itemName);
+        System.out.println("Price is " + itemPrice);
+
+    }
+}
