c2252d356b95055626c3c55a447b9f06acf09dd5
Mariia Polova <mariia.poliova@gmail.com>
2026-07-13 20:05:04 +0200

    Add string length exercise

---
+++ Introduction to Java/StringLength.java
@@ -0,0 +1,12 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String bookName = "Pride and Prejudice";
+        int bookNameLength = bookName.length();
+        int authorNameLength = "Jane Austin".length();
+
+        System.out.println("The length of the book title is " + bookNameLength);
+        System.out.println("The length of the author's name is " + authorNameLength);
+    }
+
+    
+}
