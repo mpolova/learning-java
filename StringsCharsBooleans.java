f4e6252adbb282657e683de5838f84b905b33ef5
Mariia Polova <mariia.poliova@gmail.com>
2026-07-13 12:59:32 +0200

    Add strings, chars, and booleans exercise

---
+++ StringsCharsBooleans.java
@@ -0,0 +1,11 @@
+public class StringsCharsBooleans {
+    public static void main (String [] args){
+        char tshirtSize = 'M';
+
+        boolean isOnSale = true;
+
+        String color = "green";
+
+        System.out.println("Size: " + tshirtSize + ", On sale: " + isOnSale + ", Color: " + color);
+    }
+}
