5232d4fbfa0ad793c2175bf4c824160777180b17
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 10:00:33 +0200

    Add arrays exercise

---
+++ Introduction to Java/ArraysExercise.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ArraysExercise {
+    public static void main(String[] args) {
+        String [] nobelLaureates = {"Curie", "Einstein", "Fermi"};
+        int [] years = {1903, 1921, 1938};
+
+        System.out.println(Arrays.toString(nobelLaureates));
+        System.out.println(Arrays.toString(years));
+    }
+    
+}
