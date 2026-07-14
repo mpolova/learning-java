6268e778538cc748db1950d125324fb4ff6ee29c
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 11:08:28 +0200

    Add basic maths exercise

---
+++ Introduction to Java/BasicMaths.java
@@ -0,0 +1,22 @@
+class BasicMaths {
+    public static void main(String[] args) {
+    
+    	// Save the result of 5 plus 6 into varA
+        int varA = 5+6;
+        
+        // Save the result of varA minus 3.5 into varB
+        double varB = varA-3.5;
+        
+        // Save the result of 3 times 3 into varC
+        int varC = 3*3; 
+        
+        // Save the result of varC divided by 3 into varD1
+        int varD1 = varC/3;
+        
+        // Save the result of varC divided by 3 into varD2
+        int varD2 = varC/3;   
+        
+        System.out.println("int varD1: " + varD1);
+        System.out.println("double varD2: " + varD2);
+	}
+}
