c024ceb4e701ba5c749f484c561bfc40212fb9d8
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 11:38:51 +0200

    Add a comparison exercise

---
+++ Introduction to Java/Comparisons.java
@@ -0,0 +1,13 @@
+public class Comparisons {
+    public static void main(String[] args) {
+        // Check if 5 is smaller than 7
+        boolean first = 5 < 7;
+
+        // Check if 9 is greater than or equal to 2
+        boolean second = 9 >= 2;
+
+        System.out.println("5 is smaller than 7: " + first);
+        System.out.println("9 is greater or equal to 2: " + second);
+    }
+    
+}
