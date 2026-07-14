688d842c46a1355a3bedd558869e03e840b36321
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 09:17:25 +0200

    Add upper/lower case exercise

---
+++ Introduction to Java/UpperLowerCase.java
@@ -0,0 +1,8 @@
+public class UpperLowerCase {
+    
+    public static void main(String[] args) {
+        String message = "Good morning!";
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+    }
+}
