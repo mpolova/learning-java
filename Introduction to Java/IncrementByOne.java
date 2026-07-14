80bd41f1d0850c772afcd790e9c09250216211db
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 11:22:43 +0200

    Add an increment by one exercise

---
+++ Introduction to Java/IncrementByOne.java
@@ -0,0 +1,17 @@
+public class IncrementByOne {
+    public static void main(String[] args) {
+        int age = 30;
+        int days = 20;
+        System.out.println("I'm " +  age + " roday.");
+        // Increase the age by 1
+        age++;
+
+        // Decrease the days by 1
+        days--;
+
+        System.out.println("But I'll be " +  age + " soon!");
+        System.out.println("Only " + days + " to go!");
+
+    }
+    
+}
