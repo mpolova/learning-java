7e946debb943fe0a590c5af4001566777d770a12
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 11:45:06 +0200

    Adding an equal or not exercise

---
+++ Introduction to Java/EqualOrNot.java
@@ -0,0 +1,10 @@
+public class EqualOrNot {
+    public static void main(String[] args) {
+        int a = 193;
+        // Check if 193 is equal to a
+        boolean first = a == 193;
+        // Complete the print statement
+        System.out.println("193 is equal to a: " + first);
+    }
+    
+}
