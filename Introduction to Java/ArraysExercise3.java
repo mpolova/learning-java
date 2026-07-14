e674d923aeb613f913bd66e374f54b904d47c235
Mariia Polova <mariia.poliova@gmail.com>
2026-07-14 10:38:06 +0200

    Add arrays exercise 3

---
+++ Introduction to Java/ArraysExercise3.java
@@ -0,0 +1,13 @@
+class ArraysExercise3 {
+    public static void main(String[] args) {
+
+        String[] wimbledonChamps = {"Federer", "Nadal", "Djokovic"};
+        String[] usOpenChamps = {"Sampras", "Agassi", "Nadal", "Alcaraz"};
+        String[] frenchOpenChamps = {"Nadal", "Wawrinka"};
+
+        // Print the lengths of the arrays
+        System.out.println(wimbledonChamps.length + " Wimbledon champions");
+        System.out.println(usOpenChamps.length + " US Open champions");
+        System.out.println(frenchOpenChamps.length + " French Open champions");
+    }
+}
