1b528de14374593b56e2960e0886fe97e7e57ab6
Mariia Polova <mariia.poliova@gmail.com>
2026-09-10 09:33:14 +0200

    Add code performance exercises

---
+++ 07-Optimizing Code in Java/03_Improving Code Performance/02_ImageProcessor.java
@@ -0,0 +1,42 @@
+package 07-Optimizing Code in Java.03_Improving Code Performance;
+
+class 02_ImageProcessor { 
+    public static void main(String[] args) {
+        List<Image> images = generateImages(500);
+        List<Image> result = images
+            // Use a parallel stream for multi-thread processing
+            .parallelStream()
+            // Apply filters to each image using `map()`
+            .map(image -> applyFilters(image)) 
+            // Collect images to a single list
+            .collect(Collectors.toList()); 
+    }
+
+    static Image applyFilters(Image image) {
+        image = blur(image);
+        image = sharpen(image);
+        return image;
+    }
+
+    static Image blur(Image image) {
+        process(); return new Image(image.id, image.width, image.height);
+    }
+
+    static Image sharpen(Image image) {
+        process(); return new Image(image.id, image.width, image.height);
+    }
+
+    static void process() {
+        for (int i = 0; i < 100000; i++) {
+       		double result = Math.sin(i) * Math.cos(i);
+        }
+    }
+
+    static List<Image> generateImages(int count) {
+        List<Image> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add(new Image(i, 1920, 1080));
+        }
+        return list;
+    }
+}
